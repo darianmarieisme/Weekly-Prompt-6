1368e8296f9626dfa985ac3eda6402236eb3d3da
Darian Bruce <darianmariebruce@gmail.com>
2026-02-22 14:51:33 -0500

    created header

---
+++ Weekly-Prompt-6.py
@@ -0,0 +1,6 @@
+'''Weekly Prompt 6 Option 2
+Darian Marie Bruce
+02/22/2026
+Discuss the input() function and its limitation in assigning a value to a variable. 
+What is a common cast to convert a string to another data type?
+'''
